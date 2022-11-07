1bb29cd524f2f9141eaa1a9604c778a48e2fd756
tooBusyNow <OldsmobileBuick54@yandex.ru>
2022-11-07 05:24:27 +0500

    updated revealed func

--- contracts/RPS.sol
+++ contracts/RPS.sol
@@ -10,8 +10,16 @@ contract RPS {
 
     mapping(address=>bool) public playingNow; 
     mapping(address=>bool) public alreadyRevealed;
+    mapping(address=>string) private revealedValues;
 
     event GameStarted(address, address);
+    event FinishGame(address);
+
+    modifier checkOpponent (address otherPlayer){
+        require(opponent[msg.sender] == otherPlayer || opponent[otherPlayer] == msg.sender,
+        'That is not your actual opponent');
+        _;
+    }
 
     function startGame(address otherPlayer) external {
         require(opponent[msg.sender] == address(0), 'You already have an opponent');
@@ -24,27 +32,27 @@ contract RPS {
         emit GameStarted(msg.sender, otherPlayer);
     }
 
-    function makeCommit(bytes32 hashFromVote, address otherPlayer) external {
+    function makeCommit(bytes32 hashFromVote, address otherPlayer) checkOpponent(otherPlayer) external {
         require(playingNow[msg.sender] == true, 'You have not started the game');
-        require(opponent[msg.sender] == otherPlayer || opponent[otherPlayer] == msg.sender,
-        'You can not start a round with this player');
-
         commitHashes[msg.sender] = hashFromVote;
     }
 
-    function makeReveal(string calldata actualVote, address otherPlayer) external {
-        require(opponent[msg.sender] == otherPlayer || opponent[otherPlayer] == msg.sender,
-        'That is not your actual opponent');
-
+    function makeReveal(string calldata actualVote, address otherPlayer) checkOpponent(otherPlayer) external {
         require(commitHashes[msg.sender] != 0, 'You should make a commit at first');
         require(commitHashes[otherPlayer] != 0, 'Your opponent haven\'t made a commit yet');
 
         alreadyRevealed[msg.sender] = true;
-        if (alreadyRevealed[msg.sender] && alreadyRevealed[otherPlayer]) 
-            finishGame(msg.sender, otherPlayer);
+        revealedValues[msg.sender] = actualVote;
     }
 
-    function finishGame(address pl1, address pl2) private {
-        emit GameStarted(pl1, pl2);
+    function finishGame(address otherPlayer) checkOpponent(otherPlayer) external {
+        require(alreadyRevealed[msg.sender] == true, 'You haven\'t revealed et');
+        require(alreadyRevealed[otherPlayer] == true, 'Your opponent haven\'t revealed');
+
+        /* TODO keccak and cases*/
+    }
+
+    function getKeccak(string calldata someValue) private pure returns (bytes32) {
+        return keccak256(abi.encodePacked(someValue));
     }
 }
